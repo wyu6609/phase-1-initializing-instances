a4e23ef260dc417af1f97398455e3fb5caa052c8
William Yu <88666309+wyu6609@users.noreply.github.com>
2022-02-18 17:57:02 -0500

    commit tree 0edabf172329e20ccc4e675b1c64016e278c1089

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Write your code here
+// // Write your code here
+// class dog {
+//   constructor(name, breed) {
+//     this.name = name;
+//     this.breed = breed;
+//   }
+// }
+// let bigFluffyDog1 = new dog("Buzz", "great Pyreness");
+// let bigFluffyDog2 = new dog("Woody", "labrador");
+
+class Breakfast {
+  constructor(food, drink) {
+    this.food = food;
+    this.drink = drink;
+  }
+}
+class Lunch {
+  constructor(salad, soup, drink) {
+    this.salad = salad;
+    this.soup = soup;
+    this.drink = drink;
+  }
+}
+class Dinner {
+  #dessert;
+  constructor(salad, soup, entree, dessert) {
+    this.salad = salad;
+    this.soup = soup;
+    this.entree = entree;
+    this.#dessert = dessert;
+  }
+}
